eae6c1272bd1b1da050f5c179a7313cee728ff86
Andrej03 <andrej.bachvarovski@gmail.com>
2025-03-19 19:05:34 -0400

    minor change

--- comp3097-project/Preview Content/Views/ContentView.swift
+++ comp3097-project/Preview Content/Views/ContentView.swift
@@ -8,28 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @EnvironmentObject var taskViewModel: TaskViewModel
-//    @State private var showingAddTask = false
-//    
-//    var body: some View {
-//        NavigationView {
-//            TaskListView()
-//                .navigationTitle("Advanced ToDo App - Task List")
-//                .toolbar {
-//                    ToolbarItem(placement: .navigationBarTrailing) {
-//                        Button(action: {
-//                            showingAddTask = true
-//                        }) {
-//                            Image(systemName: "plus")
-//                        }
-//                    }
-//                }
-//                .sheet(isPresented: $showingAddTask) {
-//                    AddTaskView()
-//                }
-//        }
-//    }
-//}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

a3e5b3964a28a1ac3c9ef962f60a8d216db74acf
Andrej03 <andrej.bachvarovski@gmail.com>
2025-03-19 19:03:44 -0400

    New task and task structure setup

--- comp3097-project/Preview Content/Views/AddNewTask.swift
+++ comp3097-project/Preview Content/Views/AddNewTask.swift
@@ -4,3 +4,51 @@
 //
 //  Created by Andrej Bachvarovski on 2025-03-19.
 //
+
+import SwiftUI
+
+struct AddNewTask: View {
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject var taskViewModel: TaskViewModel
+    
+    @State private var title: String = ""
+    @State private var description: String = ""
+    @State private var dueDate: Date = Date()
+    @State private var status: TaskStatus = .notFinished
+    
+    var task: Task?
+    var isEditing: Bool = false
+    
+    init(task: Task? = nil, isEditing: Bool = false) {
+        self.task = task
+        self.isEditing = isEditing
+        
+        if let task = task {
+            _title = State(initialValue: task.title)
+            _description = State(initialValue: task.description)
+            _dueDate = State(initialValue: task.dueDate)
+            _status = State(initialValue: task.status)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Task Details")) {
+                    TextField("Title", text: $title)
+                    TextField("Description", text: $description)
+                    DatePicker("Due Date", selection: $dueDate,
+                               displayedComponents: .date)
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+struct AddNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNewTask()
+           .environmentObject(TaskViewModel())
+    }
+}
--- comp3097-project/Preview Content/Views/TaskViewModel.swift
+++ comp3097-project/Preview Content/Views/TaskViewModel.swift
@@ -4,3 +4,38 @@
 //
 //  Created by Andrej Bachvarovski on 2025-03-19.
 //
+
+// The Model where we build the functionality for the task
+// CREATE, UPDATE, DELETE, SAVE ...
+
+import SwiftUI
+import Combine
+import Foundation
+
+class TaskViewModel: ObservableObject {
+    @Published var tasks: [Task] = []
+    
+    init() {
+        loadTasks()
+    }
+    
+    func createTask(title: String) {
+        
+    }
+    
+    func updateTask(id: Int, title: String) {
+        
+    }
+    
+    func deleteTask(id: Int) {
+        
+    }
+    
+    func saveTasks() {
+        
+    }
+    
+    func loadTasks() {
+        
+    }
+}

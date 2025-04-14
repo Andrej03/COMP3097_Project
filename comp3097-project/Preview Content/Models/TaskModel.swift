aad0359570cf47e63352e29cf778522b5aa6cdec
Andrej Bachvarovski <96391874+Andrej03@users.noreply.github.com>
2025-04-14 14:25:52 -0400

    model redefinition of changes

--- comp3097-project/Preview Content/Models/TaskModel.swift
+++ comp3097-project/Preview Content/Models/TaskModel.swift
@@ -5,7 +5,7 @@ enum TaskStatus: String, CaseIterable, Identifiable, Codable {
     case done = "Done"
     case inProgress = "In Progress"
     case notFinished = "Not Finished"
-    
+    // custome type should be here
     var id: String { self.rawValue }
     
     var color: Color {
@@ -21,6 +21,7 @@ enum TaskStatus: String, CaseIterable, Identifiable, Codable {
 }
 
 struct Task: Identifiable, Codable, Equatable {
+    // Initialization of ID as a fieldof UUID as previously not initiated (Andrej)
     var id: UUID = UUID()
     var title: String
     var description: String = ""

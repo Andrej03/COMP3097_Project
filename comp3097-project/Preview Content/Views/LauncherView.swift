d330ac788fc019f81f3c64e5038fcdb8bc193f09
nazneen akter nitu <150930861+nazneennitu@users.noreply.github.com>
2025-04-14 11:56:39 -0400

    timer added for launcher

--- comp3097-project/Preview Content/Views/LauncherView.swift
+++ comp3097-project/Preview Content/Views/LauncherView.swift
@@ -1,20 +1,15 @@
-//
-//  LauncherView.swift
-//  comp3097-project
-//
-//  Created by Teniel Smith-Edwards on 2025-03-20.
-//
-
 import SwiftUI
 
 struct LauncherView: View {
+    @State private var isActive = false
+
     var body: some View {
-        NavigationView {
+        if isActive {
+            TaskListView().navigationBarBackButtonHidden(true)
+        } else {
             VStack {
-                // Spacer used above and below text to push the text to the middle of the screen
                 Spacer()
-                
-                // Welcome text
+
                 Text("Welcome to Your To-Do App")
                     .font(.largeTitle)
                     .fontWeight(.bold)
@@ -23,22 +18,21 @@ struct LauncherView: View {
                     .padding()
 
                 Spacer()
-
-                // Navigates user to the Task List page - through manual navigation
-                NavigationLink(destination: TaskListView().navigationBarBackButtonHidden(true)) { // disables back button so the user cannot go back to the launcher
-                    Text("View Tasks")
-                        .font(.title)
-                        .padding()
-                        .background(Color.cyan)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                        .padding(.horizontal, 35)
-                }
-                .padding(.bottom, 50)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.black.ignoresSafeArea()) // enables the background color to black on the whole screen
+            .background(Color.black.ignoresSafeArea())
+            .onAppear {
+                // Timer to transition after 3 seconds
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
         }
     }
 }
- 
+
+#Preview {
+    LauncherView()
+} 

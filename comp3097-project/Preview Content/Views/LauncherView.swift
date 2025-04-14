1a731435a944138a9d238b85465ba5c56a952b30
Andrej Bachvarovski <96391874+Andrej03@users.noreply.github.com>
2025-04-14 14:31:52 -0400

    clearance from a previous idea

--- comp3097-project/Preview Content/Views/LauncherView.swift
+++ comp3097-project/Preview Content/Views/LauncherView.swift
@@ -21,6 +21,9 @@ struct LauncherView: View {
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color.black.ignoresSafeArea())
+            // As per the previous version, the button was switched with a 3 second timer
+            // The screen will show the launch page before opening the task list.
+            // Upgrade of the button as not it is dynamic (Andrej)
             .onAppear {
                 // Timer to transition after 3 seconds
                 DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
@@ -33,6 +36,6 @@ struct LauncherView: View {
     }
 }
 
-#Preview {
-    LauncherView()
-} 
+//#Preview {
+//    LauncherView()
+//} 
